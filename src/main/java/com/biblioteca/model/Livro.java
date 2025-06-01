eaa2fcea371612529f3e33a81f361ae26cd3f941
Guismx <guilhermesmx@gmail.com>
2025-06-01 00:11:17 -0300

    implementação classe livro

--- src/main/java/com/biblioteca/model/Livro.java
+++ src/main/java/com/biblioteca/model/Livro.java
@@ -2,4 +2,77 @@ package main.java.com.biblioteca.model;
 
 public class Livro {
 
+    private int id;
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private String editora;
+    private String isbn;
+    private boolean status;
+
+    public Livro (int id, String titulo, String autor, int anoPublicacao, String editora, String isbn, boolean status) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.editora = editora;
+        this.isbn = isbn;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
