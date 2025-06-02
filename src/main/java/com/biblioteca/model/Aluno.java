c682fd6d892591ac258032fc14a4aeec594810ad
Rayanne Matos <rayanne.contabeis@gmail.com>
2025-06-01 22:16:36 -0300

    feat: complementação da classe Usuario e ajustes nos modelos relacionados

--- src/main/java/com/biblioteca/model/Aluno.java
+++ src/main/java/com/biblioteca/model/Aluno.java
@@ -1,5 +1,17 @@
-package main.java.com.biblioteca.model;
+package com.biblioteca.model;
 
-public class Aluno {
+import com.biblioteca.enums.TipoUsuario;
+import com.biblioteca.enums.Turno;
+
+public class Aluno extends Usuario {
+    //subclasse de Usuario
+
+    //construtor da subclasse Aluno, que está herdando da classe Usuario
+    //Essa subclase irá construir um Usuario, passando todos os dados e definindo o tipo como ALUNO sem precisar setar manualmente depois
+
+
+    public Aluno(long id, String nome, String matricula, String cpf, boolean ativo, String email, Turno turno) {
+        super(id, nome, matricula, cpf, ativo, email, turno, TipoUsuario.ALUNO);
+    }
 
 }
--- src/main/java/com/biblioteca/model/Usuario.java
+++ src/main/java/com/biblioteca/model/Usuario.java
@@ -1,31 +1,42 @@
-package main.java.com.biblioteca.model;
+package com.biblioteca.model;
 
-import main.java.com.biblioteca.enums.Turno;
+import com.biblioteca.enums.TipoUsuario;
+import com.biblioteca.enums.Turno;
 
+
+/*Requisitos:
+Cadastro de usuários: nome, matrícula, tipo (aluno ou professor), CPF e e-mail*/
 public abstract class Usuario {
-    private int id;
+    private long id; //long para garantir a capacidade de persistência no banco de dados, visando a longo prazo
     private String nome;
+    private String matricula;
     private String cpf;
     private boolean ativo;
     private String email;
     private Turno turno;
+    private final TipoUsuario tipo; //não poderá ser alterado durante todo o contexto de execução
 
-    public Usuario(int id, String nome, String cpf, boolean ativo, String email, Turno turno) {
+    //Construtor Usuario
+    public Usuario(long id, String nome, String matricula, String cpf, boolean ativo, String email, Turno turno, TipoUsuario tipo) {
         this.id = id;
         this.nome = nome;
+        this.matricula = matricula;
         this.cpf = cpf;
         this.ativo = ativo;
         this.email = email;
         this.turno = turno;
+        this.tipo = tipo;
     }
 
-    public int getId() {
+    // get e set ID
+    public long getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
-    
+
+    // get e set nome
     public String getNome() {
         return nome;
     }
@@ -33,6 +44,15 @@ public abstract class Usuario {
         this.nome = nome;
     }
 
+    // get e set matricula
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    // get e set CPF
     public String getCpf() {
         return cpf;
     }
@@ -40,14 +60,15 @@ public abstract class Usuario {
         this.cpf = cpf;
     }
 
+    // get e set Turno
     public Turno getTurno() {
         return turno;
     }
-
     public void setTurno(Turno turno) {
         this.turno = turno;
     }
 
+    // get e set E-mail
     public String getEmail() {
         return email;
     }
@@ -55,11 +76,17 @@ public abstract class Usuario {
         this.email = email;
     }
 
+    // get e set Status
     public boolean isAtivo() {
         return ativo;
     }
-
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
+    }
+
+    // get Tipo de usuário
+    // Sem set para não permitir que seja alterado o tipo do usuário após a criação
+    public TipoUsuario getTipo() {
+        return tipo;
     }
 }
