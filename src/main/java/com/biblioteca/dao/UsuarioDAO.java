16fe93c217160b94b103f9345e6f22d96de0b002
Guismx <guilhermesmx@gmail.com>
2025-06-07 23:00:29 -0300

    feat: implementa método mapearUsuario e BuscarPorMatricula

--- src/main/java/com/biblioteca/dao/UsuarioDAO.java
+++ src/main/java/com/biblioteca/dao/UsuarioDAO.java
@@ -1,9 +1,14 @@
 package com.biblioteca.dao;
 
 import com.biblioteca.database.ConexaoJDBC;
+import com.biblioteca.enums.TipoUsuario;
+import com.biblioteca.enums.Turno;
+import com.biblioteca.model.Aluno;
+import com.biblioteca.model.Professor;
 import com.biblioteca.model.Usuario;
 
 import java.sql.*;
+import java.util.List;
 
 public class UsuarioDAO {
 
@@ -47,7 +52,46 @@ public class UsuarioDAO {
     }
 
     //TODO: Implementar método
-    public void ListarUsuarios() {
+    public Usuario buscarPorMatricula(String matricula) {
+        String sql = "SELECT * FROM usuario WHERE matricula = ?";
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = ConexaoJDBC.getConnection();
+            stm = conn.prepareStatement(sql);
+            stm.setString(1, matricula); // Busca pelo parâmetro String
+            rs = stm.executeQuery();
+            if (rs.next()) {
+                return mapearUsuario(rs);
+            } else {
+                System.out.println("Matricula não encontrada: " + matricula);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao consultar a matrícula: " + matricula);
+            e.printStackTrace();
+        } finally {
+            ConexaoJDBC.closeConnection(conn, stm, rs);
+        }
+        return null;
+    }
+
+    //TODO: Implementar método
+    public List<Usuario> ListarUsuarios() {
         String sql = "SELECT cpf, nome";
+        return null;
+    }
+
+    //Método privado da classe Usuario que vai servir para "montar" um objeto para ser utilizado como retorno para os outros métodos
+    private Usuario mapearUsuario(ResultSet rs) throws SQLException {
+        //Define qual construtor chamar
+        TipoUsuario tipo = TipoUsuario.valueOf(rs.getString("tipo"));
+        Usuario usuario = (tipo == TipoUsuario.ALUNO)
+                ? new Aluno(rs.getString("nome"), rs.getString("matricula"), rs.getString("cpf"), rs.getString("email"), Turno.valueOf(rs.getString("turno")))
+                : new Professor(rs.getString("nome"), rs.getString("matricula"), rs.getString("cpf"), rs.getString("email"), Turno.valueOf(rs.getString("turno")));
+        usuario.setId(rs.getLong("id"));
+        usuario.setAtivo(rs.getBoolean("ativo"));
+        return usuario;
     }
 }
+
