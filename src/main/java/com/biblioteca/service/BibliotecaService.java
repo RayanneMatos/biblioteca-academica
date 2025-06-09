f6fb26c6662d2efba2fb7fbb83ed0aeef3aacc2d
RayanneMatos <rayanne.contabeis@gmail.com>
2025-06-09 11:17:06 -0300

    Construção da BibliotecaService e ajuste em DAO

--- src/main/java/com/biblioteca/service/BibliotecaService.java
+++ src/main/java/com/biblioteca/service/BibliotecaService.java
@@ -1,7 +1,44 @@
 package com.biblioteca.service;
 
-public class BibliotecaService {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
+import com.biblioteca.database.ConexaoJDBC;
+import com.biblioteca.dao.UsuarioDAO;
+import com.biblioteca.model.Usuario;
+import com.biblioteca.model.TipoUsuario;
+
+public class BibliotecaService {
+
+    // Método genérico para cadastrar qualquer tipo de usuário
+    public void cadastrarUsuario(Usuario usuario) {
+        try (Connection con = ConexaoJDBC.getConnection()) {
+            UsuarioDAO usuarioDAO = new UsuarioDAO(con);
+            usuarioDAO.cadastrar(usuario);
+            System.out.println("Usuário cadastrado com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Usuario autenticarUsuario(String login, String senha) {
+        try (Connection con = ConexaoJDBC.getConnection()) {
+            UsuarioDAO usuarioDAO = new UsuarioDAO(con);
+            return usuarioDAO.buscarPorLoginSenha(login, senha);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Usuario> listarUsuariosPorTipo(TipoUsuario tipo) {
+        try (Connection con = ConexaoJDBC.getConnection()) {
+            UsuarioDAO usuarioDAO = new UsuarioDAO(con);
+            return usuarioDAO.listarPorTipo(tipo);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
